e936113ee2620a133d13b7a906a34696b4f325f7
marysanders <lyremary@yahoo.com>
2024-02-12 15:32:37 -0600

    Complete exercise reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let splitString = string.split("");
+  let reversedArray = splitString.reverse();
+  let newString = reversedArray.join("").toString();
+  return newString;
 };
 
 // Do not edit below this line
