7f507ac8e6ca0681a3b2812e115f93663b4aa3bb
marysanders <lyremary@yahoo.com>
2024-02-20 15:01:17 -0600

    Complete removeFromArray Exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (array, ...nums) {
+    for (const num of nums) {
+      for (let i = 0; i < array.length; i++) {
+        if (array.includes(num)) {
+          let position = array.indexOf(num);
+          array.splice(position, 1);
+        }
+      }
+    }
+  
+    return array;
+  };
+  
 
 // Do not edit below this line
 module.exports = removeFromArray;
