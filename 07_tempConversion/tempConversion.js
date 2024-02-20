2a7cc94877c184eda2d30b10820d49dc04bc2301
marysanders <lyremary@yahoo.com>
2024-02-20 16:59:54 -0600

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
-};
-
-const convertToFahrenheit = function() {
-};
-
+const convertToCelsius = function(num) {
+  let celcius = (num - 32) * 5/9;
+   let ans = Math.round(celcius * 10) / 10;
+   return ans;
+ };
+ 
+ const convertToFahrenheit = function(num) {
+   let fahrenheit = (num * 9/5 + 32);
+   let ans = Math.round(fahrenheit * 10) / 10;
+   return ans;
+ };
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
